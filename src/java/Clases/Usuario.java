31b618ca7e0b637890f38559b977a5fa9afccf86
carlox1998 <cmoralesgallego@gmail.com>
2019-11-04 09:22:29 +0100

    Clase Usuario hecha

---
+++ src/java/Clases/Usuario.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author carlox
+ */
+public class Usuario {
+    private int victorias;
+    private int derrotas;
+
+    public Usuario() {
+        
+    }
+
+    public int getVictorias() {
+        return victorias;
+    }
+
+    public void setVictorias(int victorias) {
+        this.victorias = victorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+    
+    
+    
+    
+    
+}
